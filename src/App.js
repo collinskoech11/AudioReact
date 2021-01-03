15d1cc60cc469c25971ccd536c735d09e1fc4197
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2021-01-03 16:27:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import ReactAudioPlayer from 'react-audio-player';
 function App() {
   return (
     <div className="App">
-      
+      <ReactAudioPlayer
+       src="my_audio_file.ogg"
+       autoPlay
+       controls
+     />
     </div>
   );
 }
